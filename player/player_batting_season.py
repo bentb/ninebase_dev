4de6b0f6746e389f9b13bcf9dbc39d73d1708195
Ben Burnet <b7tburnet@gmail.com>
2023-07-12 00:36:12 -0400

    'd'

--- player/player_batting_season.py
+++ player/player_batting_season.py
@@ -2,21 +2,24 @@
 # coding: utf-8
 
 import streamlit as st
-from nbconvert import PythonExporter
+import json
+from nbformat import reads, NotebookNode
 
 def batting_page():
     st.write('This is the batting page.')
 
     # Load the Jupyter notebook file
-    notebook_filename = 'data_analyze/player_batting_season.ipynb'
+    notebook_filename = 'path/to/player_batting_season.ipynb'  # Replace with the actual file path
     with open(notebook_filename, 'r') as f:
-        notebook_content = f.read()
+        notebook_content = json.load(f)
 
-    # Convert the notebook content to executable Python code
-    exporter = PythonExporter()
-    python_code, _ = exporter.from_notebook_node(notebook_content)
+    # Extract code cells from the notebook content
+    code_cells = [
+        cell.source
+        for cell in notebook_content['cells']
+        if cell.cell_type == 'code'
+    ]
 
-    # Display the code using Streamlit's code block
-    st.code(python_code, language='python')
-
-
+    # Display the code cells
+    for code_cell in code_cells:
+        st.code(code_cell, language='python')
