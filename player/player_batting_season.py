91cf41930cd399516e832b61f4f45a97f2bdaf8b
Ben Burnet <b7tburnet@gmail.com>
2023-07-12 00:54:42 -0400

    'd'

--- player/player_batting_season.py
+++ player/player_batting_season.py
@@ -4,8 +4,8 @@
 
 import streamlit as st
 from nbformat import reads
-from nbconvert import NotebookNode
-from streamlit_ace import st_ace
+from nbconvert import HTMLExporter
+import nbformat
 
 def batting_page():
     st.write('This is the batting page.')
@@ -16,24 +16,16 @@ def batting_page():
         notebook_content = f.read()
 
     # Parse the notebook content
-    notebook_node = reads(notebook_content, as_version=4)
+    notebook_node = nbformat.reads(notebook_content, as_version=4)
 
-    # Display the notebook content with code cells and outputs
-    for cell in notebook_node['cells']:
-        if cell['cell_type'] == 'code':
-            code = ''.join(cell['source'])
-            outputs = [output for output in cell.get('outputs', []) if output.get('output_type') == 'display_data']
-            st_ace(code, language='python', readonly=True, show_gutter=False, show_print_margin=False)
-            for output in outputs:
-                if 'data' in output:
-                    for key, value in output['data'].items():
-                        if key.startswith('image'):
-                            st.image(value)
-                        else:
-                            st.write(value)
+    # Convert the notebook content to HTML
+    exporter = HTMLExporter()
+    html_content, _ = exporter.from_notebook_node(notebook_node)
+
+    # Display the rendered notebook content
+    st.components.v1.html(html_content, width=800, height=600)
 
 try:
     batting_page()
-except Exception as e:
-    st.error(f"An error occurred: {e}")
+
 
