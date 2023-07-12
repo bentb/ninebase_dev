b538c3bd173c844a56263620011175b96ad8a743
Ben Burnet <b7tburnet@gmail.com>
2023-07-12 00:51:59 -0400

    'd'

--- player/player_batting_season.py
+++ player/player_batting_season.py
@@ -3,6 +3,7 @@
 #data_analyze/player_batting_season.ipynb
 
 import streamlit as st
+from nbformat import reads
 from nbconvert import HTMLExporter
 
 def batting_page():
@@ -13,9 +14,12 @@ def batting_page():
     with open(notebook_filename, 'r') as f:
         notebook_content = f.read()
 
+    # Parse the notebook content
+    notebook_node = reads(notebook_content, as_version=4)
+
     # Convert the notebook content to HTML
     exporter = HTMLExporter()
-    html_content, _ = exporter.from_notebook_node(notebook_content)
+    html_content, _ = exporter.from_notebook_node(notebook_node)
 
     # Display the rendered notebook content
     st.components.v1.html(html_content, width=800, height=600)
@@ -24,5 +28,4 @@ try:
     batting_page()
 except Exception as e:
     st.error(f"An error occurred: {e}")
-
 
