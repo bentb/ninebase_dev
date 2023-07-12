6af5385a866d49163c0d641c194d0f980864ed9c
Ben Burnet <b7tburnet@gmail.com>
2023-07-12 00:42:17 -0400

    'd'

--- player/player_batting_season.py
+++ player/player_batting_season.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+#data_analyze/player_batting_season.ipynb
 
 import streamlit as st
 import json
@@ -9,17 +10,14 @@ def batting_page():
     st.write('This is the batting page.')
 
     # Load the Jupyter notebook file
-    notebook_filename = 'data_analyze/player_batting_season.ipynb'  # Replace with the actual file path
+    notebook_filename = 'path/to/player_batting_season.ipynb'  # Replace with the actual file path
     with open(notebook_filename, 'r') as f:
         notebook_content = json.load(f)
 
-    # Extract code cells from the notebook content
-    code_cells = [
-        cell.source
-        for cell in notebook_content['cells']
-        if cell.cell_type == 'code'
-    ]
+    st.write(notebook_content)  # Print the notebook content
 
-    # Display the code cells
-    for code_cell in code_cells:
-        st.code(code_cell, language='python')
+try:
+    batting_page()
+except Exception as e:
+    st.error(f"An error occurred: {e}")
+
