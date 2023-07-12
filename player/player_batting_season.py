9f3c548b5595e40e1a6f6dbc386cd46bfe11de35
Ben Burnet <b7tburnet@gmail.com>
2023-07-12 00:51:00 -0400

    'd'

--- player/player_batting_season.py
+++ player/player_batting_season.py
@@ -3,8 +3,7 @@
 #data_analyze/player_batting_season.ipynb
 
 import streamlit as st
-from nbformat import read
-from nbconvert import MarkdownExporter
+from nbconvert import HTMLExporter
 
 def batting_page():
     st.write('This is the batting page.')
@@ -12,17 +11,18 @@ def batting_page():
     # Load the Jupyter notebook file
     notebook_filename = 'data_analyze/player_batting_season.ipynb'  # Replace with the actual file path
     with open(notebook_filename, 'r') as f:
-        notebook_content = read(f, 4)  # Specify the nbformat version (e.g., 4)
+        notebook_content = f.read()
 
-    # Convert the notebook content to Markdown
-    exporter = MarkdownExporter()
-    markdown_content, _ = exporter.from_notebook_node(notebook_content)
+    # Convert the notebook content to HTML
+    exporter = HTMLExporter()
+    html_content, _ = exporter.from_notebook_node(notebook_content)
 
     # Display the rendered notebook content
-    st.markdown(markdown_content)
+    st.components.v1.html(html_content, width=800, height=600)
 
 try:
     batting_page()
 except Exception as e:
     st.error(f"An error occurred: {e}")
+
 
